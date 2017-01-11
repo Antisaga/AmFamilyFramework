42ab9b7f9503625d516c7248cee6e1dff745deaa
arturdvorak <artur_dvorak@epam.com>
2017-01-11 14:32:30 +0300

    config.properties is added + its support

--- src/test/java/Tests/Tests.java
+++ src/test/java/Tests/Tests.java
@@ -4,39 +4,42 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import Driver.Utils;
 import Steps.Steps;
 
 public class Tests {
-	
+
 	private Steps steps;
 
-	private final String USERNAME = "QA6612";
-	private final String PASSWORD = "Password1";
-	private final String BASE_URL = "https://pipepasstoigo-qd5.ipipeline.com/default.aspx?gaid=6612";
 
-	@BeforeClass(description = "Init browser") 
+/*	private final String USERNAME = "QA6612";
+	private final String PASSWORD = "Password1";
+	private final String BASE_URL = "https://pipepasstoigo-qd5.ipipeline.com/default.aspx?gaid=6612";*/
+
+	@BeforeClass(description = "Init browser")
 	public void setUp() {
 		steps = new Steps();
+
+
 	}
-	
-	@Test 
+
+	@Test
 	public void fillCaseInfoPage() {
-		steps.loginAmFamily(BASE_URL, USERNAME, PASSWORD);
+		steps.loginAmFamily(Utils.getProperty("BASE_URL"), Utils.getProperty("USERNAME"), Utils.getProperty("PASSWORD"));
 		steps.startNewCase();
 		steps.switchToCaseInfoFrame();
-		steps.fillCaseInfoPage();	
+		steps.fillCaseInfoPage();
 	}
-	
-	@Test 
+
+	@Test
 	public void fillProposedInsuredPage() {
 		steps.switchToIGoFrame();
-		steps.fillProposedInsured();	
+		steps.fillProposedInsured();
 	}
-	
-/*	@AfterClass(description = "Stop Browser")
-	public void stopBrowser() {
-		steps.closeDriver();
-	}
-*/
+
+	/*
+	 * @AfterClass(description = "Stop Browser") public void stopBrowser() {
+	 * steps.closeDriver(); }
+	 */
 
 }
